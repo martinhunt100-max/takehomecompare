e5aacacfcb2f5bc210d6005b36e68624988e5490
Martinhunt100 <martin@digital-gravy.com>
2025-10-15 00:13:05 +0100

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,5 +1,5 @@
 import { stripe } from "@/lib/stripe";
-import { auth } from "next-auth";
+import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
