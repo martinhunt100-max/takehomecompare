ea8668341e2949780806186670b35172f3f72075
Martinhunt100 <martin@digital-gravy.com>
2025-10-16 16:26:42 +0100

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 // app/api/auth/[...nextauth]/route.ts
 export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 import { handlers } from "@/auth";
 export const { GET, POST } = handlers;
