2cec570d801b0faa57d5ddf64fba18eacc67f5a9
Martinhunt100 <martin@digital-gravy.com>
2025-10-16 17:27:19 +0100

    Delete app/api/auth/[...nextauth]/route.ts

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-// app/api/auth/[...nextauth]/route.ts
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-import { handlers } from "@/auth";
-export const { GET, POST } = handlers;
