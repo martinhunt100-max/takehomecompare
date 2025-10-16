f42560144d3a016c47654349a0f4ad40781b271e
Martinhunt100 <martin@digital-gravy.com>
2025-10-16 15:33:14 +0100

    Add runtime configuration for Node.js

--- app/advanced/page.tsx
+++ app/advanced/page.tsx
@@ -1,6 +1,7 @@
 // app/advanced/page.tsx
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+export const runtime = "nodejs";
 
 import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
