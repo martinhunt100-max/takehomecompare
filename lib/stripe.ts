9fc280299cdad8860b32a588ea56ae0d4451b5bb
Martinhunt100 <martin@digital-gravy.com>
2025-10-16 10:22:47 +0100

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,2 +1,6 @@
 import Stripe from "stripe";
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: "2023-10-16" });
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-06-20", // update from 2023-10-16
+});
+
