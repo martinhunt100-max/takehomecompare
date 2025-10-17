b4142ed20d48406e012055d50ce9ed43e329a619
Martinhunt100 <martin@digital-gravy.com>
2025-10-17 13:00:04 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-// app/layout.tsx
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "TakeHomeCompare",
-  description: "Accurate, privacy-friendly take-home pay comparisons."
+  description: "Accurate, privacy-friendly take-home pay comparisons.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
